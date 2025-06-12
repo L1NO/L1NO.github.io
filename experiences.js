b45758e372ddc7ad031e08c4cfff1d01d7885170
Ossian Lin <ossianlin@gmail.com>
2025-06-12 16:35:17 +0800

    Add files via upload

---
+++ experiences.js
@@ -0,0 +1,64 @@
+const experiences = [
+  // User Experience Design
+  {
+    category: "User Experience Design",
+    items: [
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Wireframing",
+        level: "Experienced",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Prototyping",
+        level: "Experienced",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Figma",
+        level: "Intermediate",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Usability Testing",
+        level: "Intermediate",
+      }
+    ],
+  },
+  // User Experience Research
+  {
+    category: "User Experience Research",
+    items: [
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "User Interviews",
+        level: "Experienced",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Surveys",
+        level: "Experienced",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "A/B Testing",
+        level: "Intermediate",
+      },
+      {
+        icon: "./img/checkmark.png",
+        alt: "Experience icon",
+        title: "Analytics Tools",
+        level: "Intermediate",
+      },
+    ],
+  },
+];
+
+window.experiences = experiences;
---
+++ projects.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    img: "./img/project-1.png",
+    alt: "Project 1",
+    title: "Project One",
+    github: "https://github.com/",
+    demo: "https://github.com/"
+  },
+  {
+    img: "./img/project-2.png",
+    alt: "Project 2",
+    title: "Project Two",
+    github: "https://github.com/",
+    demo: "https://github.com/"
+  },
+  {
+    img: "./img/project-3.png",
+    alt: "Project 3",
+    title: "Project Three",
+    github: "https://github.com/",
+    demo: "https://github.com/"
+  }
+];
+
+window.projects = projects;
