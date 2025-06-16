03b51ef5a03e5b9d0ebd89350659bde1e6df053d
Ossian Lin <ossianlin@gmail.com>
2025-06-16 11:48:09 +0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -3,21 +3,7 @@ const projects = [
     img: "./img/project-STEMUP.png",
     alt: "Project 1",
     title: "STEM UP: an AR English learning app",
-    read: "https://www.notion.so/lino4ntust/STEM-UP-an-AR-English-learning-app-965523ce7aa74e3798c3e5bead57790f?source=copy_link",
-  },
-  {
-    img: "./img/project-2.png",
-    alt: "Project 2",
-    title: "Project Two",
-    read: "https://github.com/",
-    demo: "https://github.com/"
-  },
-  {
-    img: "./img/project-3.png",
-    alt: "Project 3",
-    title: "Project Three",
-    read: "https://github.com/",
-    demo: "https://github.com/"
+    read: "./img/project-STEMUP.pdf",
   }
 ];
 
